b154c1509ffa810cb0f31deedc69900d4f4fbbdd
SaiyaraOishi <saiyara.oishi@gmail.com>
2021-08-06 12:28:33 +0600

    Programming Contest registration

--- controllers/programmingContest.controller.js
+++ controllers/programmingContest.controller.js
@@ -1,9 +1,9 @@
-const MathOlympiad = require("../models/MathOlympiad.model");
-const getMO = (req, res) =>{
-res.render("math-olympiad/register.ejs", {error: req.flash("error")});
+const ProgrammingContest = require("../models/ProgrammingContest.model");
+const getPC = (req, res) =>{
+res.render("programming-contest/register.ejs", {error: req.flash("error")});
 };
 
-const postMO = (req, res) =>{
+const postPC = (req,res) =>{
     const {name, category, contact, email, institution, tshirt} = req.body;
     console.log(name);
     console.log(category);
@@ -14,11 +14,11 @@ const postMO = (req, res) =>{
 
     let registrationFee = 0;
     if(category=="School"){
-        registrationFee=250;
-    }else if(category=="College"){
-        registrationFee=400;
-    }else{
         registrationFee=500;
+    }else if(category=="College"){
+        registrationFee=600;
+    }else{
+        registrationFee=800;
     }
 
    const total= registrationFee;
@@ -26,14 +26,14 @@ const postMO = (req, res) =>{
    const selected=false;
    
    let error="";
-   MathOlympiad.findOne({name:name,contact:contact}).then((participant)=>{
+   ProgrammingContest.findOne({name:name,contact:contact}).then((participant)=>{
        if(participant){
            error="Participant with thise name and contact number already exists!";
            console.log(error);
            req.flash("error", error);
-           res.redirect("/MathOlympiad/register");
+           res.redirect("/ProgrammingContest/register");
        }else{
-           const participant=new MathOlympiad({
+           const participant=new ProgrammingContest({
                name,
                category,
                contact,
@@ -49,57 +49,57 @@ const postMO = (req, res) =>{
             error="Participant has been registered successfully!";
            console.log(error);
            req.flash("error", error);
-            res.redirect("/MathOlympiad/register");
+            res.redirect("/ProgrammingContest/register");
            }).catch(()=>{
                error="An unexpected error occured while registering the participant!";
                req.flash("error", error);
-               res.redirect("/MathOlympiad/register");
+               res.redirect("/ProgrammingContest/register");
            })
        }
    })
     
- };
+}
 
- const getMOList = (req, res) =>{
-     let all_participant = [];
+const getPCList = (req,res) =>{
+    let all_participant = [];
      let error="";
-     MathOlympiad.find().then((data)=>{
+     ProgrammingContest.find().then((data)=>{
          all_participant=data;
-        res.render(math-olympiad/list.ejs,{
+        res.render(programming-contest/list.ejs,{
             error:req.flash("error"),
             participant:all_participant,
         });
 
      }).catch(()=>{
         error="Failed to fetch data!";
-        res.render("math-olympiad/list.ejs",{
+        res.render("programming-contest/list.ejs",{
             error:req.flash("error",error),
             participants:all_participant,
         });
      });
     
-};
+}
 
-const deleteMO = (req, res) => {
+const deletePC = (req,res) =>{
     let error = "";
 
-  MathOlympiad.deleteOne({ _id: req.params.id })
-    .then(() => {
-      let error = "Data has been deleted successfully!";
-      req.flash("error", error);
-      res.redirect("/MathOlympiad/list");
-    })
-    .catch(() => {
-      let error = "Failed to delete data";
-      req.flash("error", error);
-      res.redirect("/MathOlympiad/list");
-    });
-};
+    ProgrammingContest.deleteOne({ _id: req.params.id })
+      .then(() => {
+        let error = "Data has been deleted successfully!";
+        req.flash("error", error);
+        res.redirect("/ProgrammingContest/list");
+      })
+      .catch(() => {
+        let error = "Failed to delete data";
+        req.flash("error", error);
+        res.redirect("/ProgrammingContest/list");
+      });
+}
 
-const paymentDoneMO = (req, res) => {
+const paymentDonePC = (req,res) =>{
     const id = req.params.id;
 
-  MathOlympiad.findOne({ _id: id })
+  ProgrammingContest.findOne({ _id: id })
     .then((participant) => {
       participant.paid = participant.total;
       participant
@@ -107,25 +107,25 @@ const paymentDoneMO = (req, res) => {
         .then(() => {
           let error = "Payment completed successfully!";
           req.flash("error", error);
-          res.redirect("/MathOlympiad/list");
+          res.redirect("/ProgrammingContest/list");
         })
         .catch(() => {
           let error = "Data could not be updated!";
           req.flash("error", error);
-          res.redirect("/MathOlympiad/list");
+          res.redirect("/ProgrammingCcontest/list");
         });
     })
     .catch(() => {
       let error = "Data could not be updated!";
       req.flash("error", error);
-      res.redirect("/MathOlympiad/list");
+      res.redirect("/ProgrammingContest/list");
     });
-};
 
-const selectMO = (req, res) => {
+}
+const selectPC = (req,res) =>{
     const id = req.params.id;
   
-    MathOlympiad.findOne({ _id: id })
+    ProgrammingContest.findOne({ _id: id })
       .then((participant) => {
         participant.selected = true;
         participant
@@ -133,19 +133,20 @@ const selectMO = (req, res) => {
           .then(() => {
             let error = "Participant has been selected successfully!";
             req.flash("error", error);
-            res.redirect("/MathOlympiad/list");
+            res.redirect("/ProgrammingContest/list");
           })
           .catch(() => {
             let error = "Data could not be updated!";
             req.flash("error", error);
-            res.redirect("/MathOlympiad/list");
+            res.redirect("/ProgrammingContest/list");
           });
       })
       .catch(() => {
         let error = "Data could not be updated!";
         req.flash("error", error);
-        res.redirect("/MathOlympiad/list");
+        res.redirect("/ProgrammingContest/list");
       });
-  };
+}
 
-module.exports={getMO, postMO, getMOList, deleteMO, paymentDoneMO, selectMO};
+module.exports = {getPC, postPC, getPCList, deletePC, paymentDonePC, selectPC};
+
